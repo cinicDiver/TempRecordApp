76b7b9036bedcdf616a5df4cbd6e5e2b9cc0bf03
cinicDiver <oscarframirezp@hotmail.com>
2021-02-01 00:04:00 -0500

    Windows for admin features added and movement between them set.

--- app.js
+++ app.js
@@ -1,23 +1,50 @@
+//-------------- FUNCTIONS CHANGING BETWEEN WINDOWS ---------------//
+
+//Shows the window for new user registry
 function newUser(){
     document.getElementById('loginDiv').classList.add('hiddenDiv');
     document.getElementById('registerDiv').classList.remove('hiddenDiv');
 }
 
+//Shows the login window
 function showLogin(currentWin){
     document.getElementById(currentWin).classList.add('hiddenDiv');
     document.getElementById('loginDiv').classList.remove('hiddenDiv');
 }
 
+//Shows the window for temperature registry
 function showTempWin(currentWin){
     document.getElementById(currentWin).classList.add('hiddenDiv');
     document.getElementById('tempDiv').classList.remove('hiddenDiv');
 }
 
+//Shows the window for admin login
+function showAdminWIn1(currentWin){
+    document.getElementById(currentWin).classList.add('hiddenDiv');
+    document.getElementById('adminWin1').classList.remove('hiddenDiv');
+}
+
+//Shows the window for admin features
+function adminApp(){
+    document.getElementById('adminWin1').classList.add('hiddenDiv');
+    document.getElementById('adminWin2').classList.remove('hiddenDiv');
+}
+
+//Gets out from the admin feature window
+function logoutAdmin(){
+    document.getElementById('adminWin2').classList.add('hiddenDiv');
+    document.getElementById('loginDiv').classList.remove('hiddenDiv');
+}
+
+//----------- FUNCTIONS CREATING USERS OR ADDING TEMPERATURE VALUES ------------//
+
+//Registers a new user for the team specified
 function registerUser(){
     console.log('New user registered.')
     showTempWin('registerDiv');
 }
 
+//Registers a temperature for a said user
 function registerTemp(){
     let userVal = document.getElementById('idValue').value;
     let dateVal = document.getElementById('dateValue').value;
@@ -26,6 +53,12 @@ function registerTemp(){
     showLogin('tempDiv');
 }
 
+//Resets the inputs
 function deleteInputs(){
+    document.getElementById('idValue').value='';
+    document.getElementById('dateValue').value='';
+    document.getElementById('tempValue').value='';
     console.log('Inputs deleted.')
 }
+
+//-------------- FUNCTIONS FOR ADMIN FEATURES ---------------//
